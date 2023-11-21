11abe0a2304b7bcd2a768b6eebe6be1d3dceef4e
hivancd <111187629+hivancd@users.noreply.github.com>
2023-11-21 14:42:56 -0500

    mejoras en maquetacion

--- scripts/change_theme.js
+++ scripts/change_theme.js
@@ -1,4 +1,4 @@
-document.getElementById("mode").addEventListener("click",()=>changeMode());
+document.getElementById("mode").addEventListener("click", () => changeMode());
 
 const streamer_imgs = document.getElementsByClassName("streamer_img");
 let imgs = [];
@@ -11,26 +11,28 @@ function changeMode() {
     let mode = change_mode_button.value;
     const r = document.querySelector(":root");
     const rs = getComputedStyle(r);
-    if(mode=="light"){
-        r.style.setProperty("--general_background_color",'white');
-        r.style.setProperty("--text_color",'#000000');
-        r.style.setProperty("--panel_background_color",'azure');
-        r.style.setProperty("--onhover_color",'blueviolet');
-        r.style.setProperty("--special_color_1","chocolate");
-        r.style.setProperty("--another_background_color","rgb(216, 226, 226)");
-        r.style.setProperty("--background_image","url(../images/iron_harvest_isegrim-removebg.png)");
+    if (mode == "light") {
+        r.style.setProperty("--general_background_color", 'rgb(240,242,244)');
+        r.style.setProperty("--text_color", 'rgb(33,39,46)');
+        r.style.setProperty("--panel_background_color", 'rgb(232,234,236)');
+        r.style.setProperty("--onhover_color", 'rgb(196,156,255)');
+        r.style.setProperty("--special_color_1", "rgb(234,114,44)");
+        r.style.setProperty("--another_background_color", "rgb(255, 255, 255)");
+        r.style.setProperty("--center_color", "rgb(125,52,238)");
+        r.style.setProperty("--background_image", "url(../images/fornite2-removebg-preview.png)");
         change_mode_button.value = "dark";
-        change_mode_button.innerHTML = "Change to " + change_mode_button.value +" Mode";
+        change_mode_button.innerHTML = "Change to " + change_mode_button.value + " Mode";
     }
-    if(mode=="dark"){
-        r.style.setProperty("--general_background_color",'#000000');
-        r.style.setProperty("--text_color",'azure');
-        r.style.setProperty("--panel_background_color",'rgb(19,18,28)');
-        r.style.setProperty("--onhover_color",'#fe8944');
-        r.style.setProperty("--special_color_1","blueviolet");
-        r.style.setProperty("--another_background_color","rgb(33, 39, 47)");
-        r.style.setProperty("--background_image","url(../images/iron_harvest_straznik-removebg.png)");
+    if (mode == "dark") {
+        r.style.setProperty("--general_background_color", 'rgb(25,32,39)');
+        r.style.setProperty("--text_color", 'rgb(254,254,255)');
+        r.style.setProperty("--panel_background_color", 'rgb(32,38,46)');
+        r.style.setProperty("--onhover_color", 'rgb(254,137,68)');
+        r.style.setProperty("--special_color_1", "rgb(125,52,237)");
+        r.style.setProperty("--another_background_color", "rgb(33, 39, 47)");
+        r.style.setProperty("--center_color", "rgb(254,137,68)");
+        r.style.setProperty("--background_image", "url(../images/fortnite3-removebg-preview.png)");
         change_mode_button.value = "light";
-        change_mode_button.innerHTML = "Change to " + change_mode_button.value +" Mode";
+        change_mode_button.innerHTML = "Change to " + change_mode_button.value + " Mode";
     }
 }
